880bef1cf0ea0090da4f02a61316916f989c9600
lr-2026 <lrmassas@syr.edu>
2026-09-17 15:45:08 +0000

    UPDATES

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,6 @@ import streamlit as st
 HW1 = st.Page('HW/HW1.py', title='HW1', icon=':material/looks_one:')
 HW2 = st.Page('HW/HW2.py', title='HW2', icon=':material/looks_two:')
 HW3 = st.Page('HW/HW3.py', title='HW3', icon=':material/looks_3:')
-pg = st.navigation([HW3, HW2, HW1])  
+HW4 = st.Page('HW/HW4.py', title='HW4', icon=':material/looks_4:')
+pg = st.navigation([HW4, HW3, HW2, HW1])  
 pg.run()
